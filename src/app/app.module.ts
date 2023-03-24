9098fa0ece8ac663062e6c72d96c62931d30199a
Parclan <molinajimenez495@gmail.com>
2023-03-24 11:46:30 -0500

    Exitoso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes , RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+
+const routes: Routes = [
+  {
+    path: 'component',
+    component: ComponentComponent,
+
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
+
+  exports:
+  [RouterModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
